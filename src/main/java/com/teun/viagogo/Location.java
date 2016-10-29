7d2ba9c007f11921fc9988cf21ae79c10b8f048e
Teun Kokke <teunk1@gmail.com>
2016-10-29 11:51:25 +0100

    Location now contains own x- and y- coordinates, including appropriate getters and setters for these and a stringify method

--- src/main/java/com/teun/viagogo/Location.java
+++ src/main/java/com/teun/viagogo/Location.java
@@ -4,5 +4,26 @@ package com.teun.viagogo;
  * Created by Teun on 10/29/2016.
  */
 public class Location {
+    private int x;
+    private int y;
 
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }

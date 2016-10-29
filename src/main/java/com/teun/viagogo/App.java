a4a56c834921fff6240a99ff86acab34c12df628
Teun Kokke <teunk1@gmail.com>
2016-10-29 11:20:21 +0100

    Added Maven Quickstart setup

---
+++ src/main/java/com/teun/viagogo/App.java
@@ -0,0 +1,13 @@
+package com.teun.viagogo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

e6cc7d077cfecaad47b766c3d5ea5f1c22c45acb
Teun Kokke <teunk1@gmail.com>
2016-10-29 11:36:43 +0100

    Rewrote AppTest class to my own clean startup version

--- src/test/java/com/teun/viagogo/AppTest.java
+++ src/test/java/com/teun/viagogo/AppTest.java
@@ -1,38 +1,15 @@
 package com.teun.viagogo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Created by Teun on 10/29/2016.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void shouldReturnHello() {
+        assertEquals("hello", "hello");
     }
 }

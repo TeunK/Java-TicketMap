d2687cc4a08ca30dbb03e1694a94508347452779
Teun Kokke <teunk1@gmail.com>
2016-10-29 17:05:01 +0100

    Added mock data generator class

---
+++ src/main/java/com/teun/viagogo/MockDataGenerator.java
@@ -0,0 +1,25 @@
+package com.teun.viagogo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by Teun on 10/29/2016.
+ */
+public class MockDataGenerator {
+    public List<Ticket> generateTickets(int amount, int priceRange) {
+        List<Ticket> tickets = new ArrayList<Ticket>();
+        for (int i=0; i<amount; i++) {
+            tickets.add(new RegularTicket(generateRandomPrice(priceRange)));
+        }
+
+        return tickets;
+    }
+
+    private double generateRandomPrice(int maxCents) {
+        Random random = new Random();
+        double cents = (1 + random.nextInt(maxCents));
+        return (cents / 100.0);
+    }
+}

71672118ea76d223d4de1c71e8274930091f0180
Teun Kokke <teunk1@gmail.com>
2016-10-29 11:49:51 +0100

    Refactored Events to keep track of their location and tickets, as opposed to Location keeping track of events

--- src/main/java/com/teun/viagogo/Event.java
+++ src/main/java/com/teun/viagogo/Event.java
@@ -1,7 +1,16 @@
 package com.teun.viagogo;
 
+import java.util.List;
+
 /**
  * Created by Teun on 10/29/2016.
  */
 public class Event {
+    private final int id;
+    private final List<Ticket> tickets;
+    private Location location;
+
+    public Event(Location location, List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 }
--- src/main/java/com/teun/viagogo/Location.java
+++ src/main/java/com/teun/viagogo/Location.java
@@ -4,4 +4,5 @@ package com.teun.viagogo;
  * Created by Teun on 10/29/2016.
  */
 public class Location {
+
 }

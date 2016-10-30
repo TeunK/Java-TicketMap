e4ed26323cffd7bc788a20717fff712d7ab33ce4
Teun Kokke <teunk1@gmail.com>
2016-10-30 20:01:17 +0000

    Added integer parse exception message when receiving user-input coordinates that do not represent integer values

--- src/main/java/com/teun/viagogo/Util.java
+++ src/main/java/com/teun/viagogo/Util.java
@@ -5,12 +5,15 @@ package com.teun.viagogo;
  */
 public class Util {
     public static Location stringToLocation(String request) {
-        //todo: assert sensible input
         if (isCoordinateString(request)) {
-            int x = Integer.parseInt(request.substring(0, request.indexOf(',')));
-            int y = Integer.parseInt(request.substring(request.indexOf(',')+1, request.length()));
+            try {
+                int x = Integer.parseInt(request.substring(0, request.indexOf(',')));
+                int y = Integer.parseInt(request.substring(request.indexOf(',')+1, request.length()));
 
-            return new Location(x, y);
+                return new Location(x, y);
+            } catch (NumberFormatException e){
+                throw new IllegalArgumentException("invalid coordinate received, axis must represent integer values");
+            }
         } else {
             throw new IllegalArgumentException("Invalid coordinate received, must be of format: x,y");
         }
@@ -19,7 +22,7 @@ public class Util {
     private static boolean isCoordinateString(String str) {
         if (str.contains(",")) {
             String cord1 = str.substring(0, str.indexOf(','));
-            String cord2 = str.substring(0, str.indexOf(','));
+            String cord2 = str.substring(str.indexOf(',')+1, str.length());
 
             if (cord1.length()>0 && cord2.length()>0) {
                 return true;
